fcb96a393b8f7554eec045e69986e0d1bb0b672a
Carlos Martell Ayala <ce_martell@hotmail.com>
2016-06-03 18:12:44 -0700

    Add files via upload

---
+++ GetAmazonPrices.py
@@ -0,0 +1,21 @@
+import bs4, requests, pyperclip, fake_useragent
+
+def getAmazonPrice(productUrl):
+    user_agent = {'User-Agent': fake_useragent.UserAgent().random}
+    
+    res = requests.get(productUrl, headers = user_agent)
+    res.raise_for_status()
+
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    
+    price = soup.select('#priceblock_ourprice')
+    title = soup.select('#productTitle')
+
+    return str('The price for ' + title[0].text.strip() + ' is ' + price[0].text.strip())
+
+amazonLink = pyperclip.paste()
+
+if 'amazon' not in amazonLink:
+    raise NameError('Not Amazon Link')
+
+print(getAmazonPrice(amazonLink))
---
+++ PascalTriangle.py
@@ -0,0 +1,19 @@
+#! python3
+
+def PascalTriangle(n):
+    triangle = []
+    for row in range(n):
+        triangle.append([])
+        if row == 0:        
+            for column in range(n):
+                triangle[row].append(1)
+        else:
+            for column in range(n-row):
+                if column == 0:
+                    triangle[row].append(1)
+                else:
+                    triangle[row].append(triangle[row-1][column]+triangle[row][column-1])
+    return triangle
+
+for row in PascalTriangle(10):
+    print (row)
